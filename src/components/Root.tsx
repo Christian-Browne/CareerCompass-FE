2c6bab4e110a946c5ae86370b59c0d9bd9ad38c7
cbrownea <ChristianBrowne33@gmail.com>
2023-08-12 22:05:57 -0700

    added nav icons

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -2,6 +2,11 @@ import { Outlet, useNavigate } from 'react-router';
 import styles from './root.module.css';
 import { Link } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
+import HomeIcon from '@mui/icons-material/Home';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PersonIcon from '@mui/icons-material/Person';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 function Root() {
   const navigate = useNavigate();
@@ -28,18 +33,23 @@ function Root() {
             </li>
           </Link>
           <Link to={'/'} className={styles.link}>
+            <HomeIcon />
             <li className={styles.navItem}>Home</li>
           </Link>
           <Link to={'/dashboard'} className={styles.link}>
+            <DashboardIcon />
             <li className={styles.navItem}>Dashboard</li>
           </Link>
           <Link to={'/demo'} className={styles.link}>
+            <PlayArrowIcon />
             <li className={styles.navItem}>Demo</li>
           </Link>
           <Link to={'/login'} className={styles.link}>
+            <PersonIcon />
             <li className={styles.navItem}>Login</li>
           </Link>
           <Link to={'/signup'} className={styles.link}>
+            <PersonAddIcon />
             <li className={styles.navItem}>Signup</li>
           </Link>
         </div>
